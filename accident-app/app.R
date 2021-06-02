c4bf6933d8171824060e4e05121916bccf97e6e5
Michael Helbraun <mhelb@microsoft.com>
2021-06-02 16:28:19 -0600

    Purple slider

--- accident-app/app.R
+++ accident-app/app.R
@@ -94,11 +94,11 @@ server <- function(input, output) {
   
   output$prediction <- renderText({pred()})
   
-  # Blue output bar
+  # Purple output bar
   output$barchart <- renderPlot({
     p <- pred()
     pp <- formatC(p, format="f", digits=2, width=5)
-    barplot(p, ylim=c(0,100), ylab="Probability (%)", col="#0A00AA", names.arg=pp, cex.names=2.5)
+    barplot(p, ylim=c(0,100), ylab="Probability (%)", col="#AA00AA", names.arg=pp, cex.names=2.5)
   })
 }
 
